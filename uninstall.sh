a4310ef6a1c6efd4703d3cc26dda875c761cc3a8
Pascal Klesse <pascal.klesse@lenne.tech>
2024-04-30 23:31:50 +0200

    fix script

--- uninstall.sh
+++ uninstall.sh
@@ -9,4 +9,5 @@ rm -rf monitory-agent/
 rm /etc/systemd/system/monitory-agent.service
 rm /usr/bin/monitory_install
 rm /usr/bin/monitory_update
+rm /usr/bin/monitory_reconfigure
 rm /usr/bin/monitory_uninstall
