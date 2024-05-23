22b4b1532dba94adeff90522519b148fc784d724
Pascal Klesse <pascal.klesse@lenne.tech>
2024-05-24 00:40:41 +0200

    wip

--- metrics/fs-size.ts
+++ metrics/fs-size.ts
@@ -1,7 +1,11 @@
 import * as si from "systeminformation";
 
 export async function fsSizeInfo() {
-    const fileSize = await si.fsSize('/');
-    console.debug('fileSize', fileSize)
-    return false;
+    const fileSize = await si.fsSize();
+    const mount = fileSize.find((fs: any) => fs.mount === '/');
+
+    console.debug('fileSize', mount);
+    return {
+        fsSize: mount ?? null
+    };
 }
